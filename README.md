1661c62ef7983728926e06189990839ba135a5b5
Margaux-Ccd <margaux.cochard@gmail.com>
2025-04-04 19:56:37 +0200

    Clear and add documentation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Satellite_glacier
 This repository allows to download and analyze satellite data for glacier dynamics study.
+
+
+It can be used if one wants to download satellite data using Google Earth Engine API.
+
+Parameters are dates, bands, satellite dataset, area of interest...
+
+
+The script at format jupyternotebook is in the @Script folder.
--- draft/Copernicus/main.py
+++ draft/Copernicus/main.py
@@ -1,13 +1,10 @@
-from datetime import date, timedelta
-import requests
-import geopandas as gpd
-import os
-import json
-from shapely.geometry import shape, Polygon
-import tqdm
-import pandas as pd
+"""This file was an attempt to download sentinel data directly from Copernicus Data Space Ecosystem
+It is however unable to filter the bands or resolution from the metadata. This code permits to download a 
+.SAFE file (which is huge) as a .zip file. It takes around 10 min to download one .SAFE file."""
 
-from keycloak import get_keycloak
+from datetime import date, timedelta
+import json
+
 from AOI_polygon import load_polygon_from_geojson
 from Search_dataset import request_dataset
 from download_safe import download_all_safe
@@ -38,12 +35,6 @@ def main():
     yesterday = today - timedelta(days=3)
     yesterday_string = yesterday.strftime("%Y-%m-%d")
     resolution_value = 10  # Example resolution in meters
-    
-    
-
-    # Construct the query URL with additional filters
-    bands_list = ['B02', 'B03', 'B04']  # Example bands list
-
 
     # Actions----------------#
     p=request_dataset(data_collection, ft, yesterday_string, today_string)
@@ -51,33 +42,7 @@ def main():
     print(tile_footprints)
     # print(p.keys)
     
-    from filter_data import get_file_list_from_server, filter_granule_img_data
-    # Example Usage
-    test_product_data = pd.DataFrame([
-    {
-        "Id": "22e2fbfe-0aa7-423d-b0b5-df46527f03f5",  # Example product ID
-        "Name": "S2A_MSIL2A_20240320T103021_N0500_R092_T31TGL_20240320T140053.SAFE",
-        "GeoFootprint": {"type": "Polygon", "coordinates": [[[0,0], [1,0], [1,1], [0,1], [0,0]]]}  # Example geo-coordinates
-    }
-    ])
-
-        # Function to filter for only 10m resolution and B03 band
-    def filter_b03_10m(file_list):
-        return filter_granule_img_data(file_list, res_value=10, band="B03")
-
-    # Run the function to get file list
-    file_dict = get_file_list_from_server(test_product_data, username, password, node_filter=filter_b03_10m)
-
-    # Print results
-    for product_id, files in file_dict.items():
-        print(f"\n✅ Product ID: {product_id}")
-        print(f"🔹 Files Found: {files}")
-
-        if files:
-            print(f"✅ The B03 band at 10m resolution was found: {files[0]}")
-        else:
-            print("❌ No matching files found.")
-    #download_all_safe(p,username, password, output_path)
+    download_all_safe(p,username, password, output_path)
 
    
 
